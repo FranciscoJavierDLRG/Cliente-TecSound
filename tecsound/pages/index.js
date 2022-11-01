1b8c6d782db35341c69bf3875af3b7061c8df292
FranciscoDRG <pancho221299@gmail.com>
2022-11-01 12:10:33 -0500

    .

--- tecsound/pages/index.js
+++ tecsound/pages/index.js
@@ -7,28 +7,39 @@ export default function Home() {
   const refForm  = useRef(null)
   const refEmail  = useRef(null)
   const refPass  = useRef(null)
-  
+  //const guardar = (e)=>{}
   const guardar = (e)=>{
     e.preventDefault();
-    console.log(refEmail.current.value);
-    console.log(refPass.current.value);
-    //192.168.39.106
-    
+      fetch('', {
+          method: 'POST',
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+              email: refEmail.current.value,
+              password: refPass.current.value,
+            })
+      })
+      .then(res => res.json())
+      .then(res=> {
+          window.localStorage.setItem("token", res.token)
+      })
+      .catch(res => console.log())
   }
+  
+  const registrarse = (e)=>{}
   return (
   <div className="container mx-auto flex flex-col items-center">
-    <form ref={refForm} class="shadow-lg w-80 p-4 flex flex-col bg-white rounded-lg">
+    <form ref={refForm} className="shadow-lg w-80 p-4 flex flex-col bg-white rounded-lg">
       <a className="text-blue-400 text-center my-2">Ingresar</a>
-      <input ref={refEmail} type="text" placeholder="Email" class="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500" required/>
-      <input ref={refPass} type="password" placeholder="Pasword" class="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500" minLength={8} maxLength={8} required/>
+      <input ref={refEmail} type="text" placeholder="Email" className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500" required/>
+      <input ref={refPass} type="password" placeholder="Pasword" className="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-cyan-500" minLength={8} maxLength={8} required/>
       <button onClick={guardar} className="w-full bg-blue-500 text-white p-3 rounded-lg font-semibold text-lg">Login</button>
       <a>""</a>
       <hr />
       <a className="text-blue-400 text-center my-2">O crea una nueva cuenta</a>
-      <button className="w-full bg-green-400 mt-8 mb-4 text-white p-3 rounded-lg font-semibold text-lg">Create New Account</button>
+      <button onClick={registrarse} className="w-full bg-green-400 mt-8 mb-4 text-white p-3 rounded-lg font-semibold text-lg">Crear nueva cuenta</button>
     </form>
   </div>
   )
 }
-
-
